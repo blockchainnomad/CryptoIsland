7152407e05d02ca9a7aa8b6d55a2bf2f513afe05
blockchainnomad <40536266+blockchainnomad@users.noreply.github.com>
2021-08-05 21:53:06 +0900

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const mnemonic = "your wallet mnemonic"
+const mnemonic = "walk tiger leopard beauty exclude tree write inhale nice just foster gown"
 module.exports = {
   compilers: {
     solc: {
@@ -14,7 +14,7 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/your project id")
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/a269b3fad4fc4036a44bd481643c4092")
       },
       network_id: 3,
       gas: 6721975,
