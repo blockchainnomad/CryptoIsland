a3be97564d9c6b7765db0afe2fe3598a170ba26e
blockchainnomad <40536266+blockchainnomad@users.noreply.github.com>
2022-08-08 15:47:55 +0900

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -17,34 +17,35 @@ App = {
       }
     });
 
-    return await App.metamaskIntalled();
+    return await App.initWeb3();
+    // return await App.metamaskIntalled();
   },
 
-  metamaskIntalled: function () {
-    if (window.ethereum) {
-      handleEthereum();
-    } else {
-      window.addEventListener('ethereum#initialized', handleEthereum, {
-        once: true,
-      });
+  // metamaskIntalled: function () {
+  //   if (window.ethereum) {
+  //     handleEthereum();
+  //   } else {
+  //     window.addEventListener('ethereum#initialized', handleEthereum, {
+  //       once: true,
+  //     });
 
-      // If the event is not dispatched by the end of the timeout,
-      // the user probably doesn't have MetaMask installed.
-      setTimeout(handleEthereum, 3000); // 3 seconds
-    }
+  //     // If the event is not dispatched by the end of the timeout,
+  //     // the user probably doesn't have MetaMask installed.
+  //     setTimeout(handleEthereum, 3000); // 3 seconds
+  //   }
 
-    function handleEthereum() {
-      const { ethereum } = window;
-      if (ethereum && ethereum.isMetaMask) {
-        console.log('Ethereum successfully detected!');
-        // Access the decentralized web!
-      } else {
-        console.log('Please install MetaMask!');
-        window.open().location.replace("https://metamask.io");
-      }
-      return App.initWeb3();
-    }
-  },
+  //   function handleEthereum() {
+  //     const { ethereum } = window;
+  //     if (ethereum && ethereum.isMetaMask) {
+  //       console.log('Ethereum successfully detected!');
+  //       // Access the decentralized web!
+  //     } else {
+  //       console.log('Please install MetaMask!');
+  //       window.open().location.replace("https://metamask.io");
+  //     }
+  //     return App.initWeb3();
+  //   }
+  // },
 
   initWeb3: async function () {
     // Modern dapp browsers...
