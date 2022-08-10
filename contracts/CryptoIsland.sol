8b8fc5a0d7754fdcaeeb05de4ab840f094df151a
blockchainnomad <40536266+blockchainnomad@users.noreply.github.com>
2022-08-10 14:59:58 +0900

    1.9.2

--- contracts/CryptoIsland.sol
+++ contracts/CryptoIsland.sol
@@ -19,57 +19,18 @@ contract CryptoIsland is
     PausableUpgradeable,
     OwnableUpgradeable
 {
-    // using SafeMathUpgradeable for uint256;
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
 
-    // CountersUpgradeable.Counter private _tokenIds;
-
-    // uint256 public constant MAX_SUPPLY = 1000;
-    // uint256 public constant PRICE = 0.01 ether;
-    // uint256 public constant MAX_PER_MINT = 1;
-
-    // string public baseTokenURI;
-
     function initialize() public initializer {
+        __ERC721_init("CryptoIsland", "MOON");
         __ERC721Enumerable_init();
         __ERC721URIStorage_init();
         __ERC721Burnable_init();
         __Pausable_init();
         __Ownable_init();
     }
-
-    // function mintNFTs(uint256 _count) public payable {
-    //     uint256 totalMinted = _tokenIds.current();
-
-    //     require(totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs left!");
-    //     require(
-    //         _count > 0 && _count <= MAX_PER_MINT,
-    //         "Cannot mint specified number of NFTs."
-    //     );
-    //     require(
-    //         msg.value >= PRICE.mul(_count),
-    //         "Not enough ether to purchase NFTs."
-    //     );
-
-    //     for (uint256 i = 0; i < _count; i++) {
-    //         _mintSingleNFT();
-    //     }
-    // }
-
-    // function reserveNFTs() public onlyOwner {
-    //     uint256 totalMinted = _tokenIds.current();
-
-    //     require(
-    //         totalMinted.add(10) < MAX_SUPPLY,
-    //         "Not enough NFTs left to reserve"
-    //     );
-
-    //     for (uint256 i = 0; i < 10; i++) {
-    //         _mintSingleNFT();
-    //     }
-    // }
 
     function safeMint(address to) public payable {
         _safeMint(to, _tokenIdCounter.current());
@@ -85,10 +46,6 @@ contract CryptoIsland is
             "https://bafybeichhwaivvgtgd6egvkpw274gri44je5cun4aelroxssd2xnlk4fxa.ipfs.nftstorage.link/";
     }
 
-    // function _baseURI() internal view virtual override returns (string memory) {
-    //     return baseTokenURI;
-    // }
-
     function tokenURI(uint256 tokenId)
         public
         view
@@ -97,10 +54,6 @@ contract CryptoIsland is
     {
         return super.tokenURI(tokenId);
     }
-
-    // function setBaseURI(string memory _baseTokenURI) public onlyOwner {
-    //     baseTokenURI = _baseTokenURI;
-    // }
 
     function _beforeTokenTransfer(
         address from,
@@ -125,12 +78,6 @@ contract CryptoIsland is
     {
         return super.supportsInterface(interfaceId);
     }
-
-    // function _mintSingleNFT() private {
-    //     uint256 newTokenID = _tokenIds.current();
-    //     _safeMint(msg.sender, newTokenID);
-    //     _tokenIds.increment();
-    // }
 
     function tokensOfOwner(address _owner)
         external
